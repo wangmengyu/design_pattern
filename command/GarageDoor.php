e0297cb16bae54a6bd635e625cdbf1738dfa3a7c
mengyu.wang <mengyu.wang.0124@gmail.com>
2018-02-27 10:34:01 +0800

    COMMAND

---
+++ command/GarageDoor.php
@@ -0,0 +1,18 @@
+<?php
+namespace DesignPatern\Command;
+class GarageDoor{
+    public function up(){
+
+    }
+    public function down(){
+
+    }
+    public function lightOn(){
+        var_dump('车库灯开了');
+    }
+    public function lightOff(){
+
+    }
+
+
+}
--- command/RemoteControlTest.php
+++ command/RemoteControlTest.php
@@ -5,15 +5,21 @@ require_once __DIR__.'/Light.php';
 require_once __DIR__.'/LightOnCommand.php';
 require_once __DIR__.'/SimpleRemoteControl.php';
 require_once __DIR__.'/Command.php';
+require_once __DIR__.'/GarageDoor.php';
+require_once __DIR__.'/GarageDoorOpenCommand.php';
 /**
- * 遥控器测试
+ * 遥控器测试 - 客户端
  * Created by PhpStorm.
  * User: wangmengyu
  * Date: 18-2-27
  * Time: 上午10:02
  */
-$remote = new SimpleRemoteControl();
-$light = new Light();
-$command = new LightOnCommand($light);
-$remote->setCommand($command);
-$remote->buttonWasPassed();
+$remote = new SimpleRemoteControl();//遥控器：调用者
+$light = new Light();//灯：接收者
+$command = new LightOnCommand($light);//开灯命令对象
+$garageDoor = new GarageDoor();
+$garageCommand = new GarageDoorOpenCommand($garageDoor);
+$remote->setCommand($command);//调用者设置好命令1：开灯命令
+$remote->buttonWasPassed();//执行命令
+$remote->setCommand($garageCommand);//调用者设置命令2：开车库灯命令
+$remote->buttonWasPassed();//执行命令
